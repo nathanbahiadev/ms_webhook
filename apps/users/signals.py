f328098a3e670f4e6ed94cabf687ee8b5ada33a8
nathanbahiadev <nathan@unidasolucoes.com.br>
2022-08-18 10:12:57 -0300

    cria testes para signal create_token

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,10 +1,9 @@
-from rest_framework.authtoken.models import Token
-from django.db.models.signals import post_save
-from django.conf import settings
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+@receiver(post_save, sender=get_user_model())
+def create_token(sender, instance, **kwargs):
+    return Token.objects.get_or_create(user=instance)
---
+++ apps/users/tests/signals/test_create_token.py
@@ -0,0 +1,32 @@
+import pytest
+from model_bakery import baker
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+from apps.users.signals import create_token
+
+
+@pytest.fixture
+def user():
+    return baker.make(get_user_model())
+
+
+@pytest.mark.django_db
+def test_create_token(user):
+    token = Token.objects.filter(user=user).exists()
+    assert token is True
+
+
+@pytest.mark.django_db
+def test_create_token_with_existing_user_must_not_create_another_token(user):
+    token, created = create_token(sender=get_user_model(), instance=user)
+    assert created is False
+    assert Token.objects.get(key=token).user == user
+
+
+@pytest.mark.django_db
+def test_create_token_with_user_without_token_must_create_a_token(user):
+    Token.objects.filter(user=user).delete()
+    token, created = create_token(sender=get_user_model(), instance=user)
+    assert created is True
+    assert Token.objects.get(key=token).user == user
